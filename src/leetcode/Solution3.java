1fe088d518175f6209433dc375cd636b7571deed
JackLinkai <2716228597@qq.com>
2021-02-08 22:56:43 +0800

    打卡第三天

--- src/leetcode/Solution3.java
+++ src/leetcode/Solution3.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -38,7 +36,7 @@ import java.util.Map;
 public class Solution3 {
     public static void main(String[] args) {
         Solution3 solution3 = new Solution3();
-        int nums = solution3.lengthOfLongestSubstring("wobgrovw");
+        int nums = solution3.lengthOfLongestSubstring1("wobgrovw");
         System.out.println(nums);
     }
 
@@ -92,5 +90,38 @@ public class Solution3 {
             return max;
         }
         return 0;
+    }
+
+
+    /**
+     * 思路：滑动窗口
+     * 1. 窗口occ用HashSet实现
+     * 2.
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring1(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        // ans统计最长字串
+        int rk = -1, ans = 0;
+        // 如果
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            // 当下一个字符在字符串中并且窗口中没有该字符
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(++rk));
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
     }
 }
