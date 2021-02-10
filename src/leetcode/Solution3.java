e8fabda1b28bfbd93d9bcbd3f56c067652f7d936
JackLinkai <2716228597@qq.com>
2021-02-10 23:25:38 +0800

    打卡第四天，重新优化最长子串问题，完事开头难，加油

--- src/leetcode/Solution3.java
+++ src/leetcode/Solution3.java
@@ -1,6 +1,7 @@
 package leetcode;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -36,92 +37,69 @@ import java.util.*;
 public class Solution3 {
     public static void main(String[] args) {
         Solution3 solution3 = new Solution3();
-        int nums = solution3.lengthOfLongestSubstring1("wobgrovw");
+        int nums = solution3.lengthOfLongestSubstring("wobgrovw");
         System.out.println(nums);
     }
 
     /**
      * 思路：滑动窗口
-     * 如果字符串为空，返回0，否则
-     * 1. 创建一个窗口map，key为字符，value为下标
-     * 2. 创建一个数max，记录最大值
-     * 3. 把字符数组第一个字符添加到map中
-     * 4. 依次遍历字符数组
-     * 5. 如果遍历到的字符在map中，删掉map中第一个元素，直到该字符不存在map中
-     * 6. 如果遍历到的字符不在map中，将其添加到map中
-     * 7. 将目前最长字串的长度保存在max中
+     * 如果数组为空，返回0
+     * 如果数组的长度小于2，返回数组长度。
+     * 1. 用HashSet实现保存字符的滑动窗口win；
+     * 2. 准备一个变量max统计出现的最长子串长度；
+     * 3. 准备两个变量，一个是窗口的左端left，一个是窗口的右端right；
+     * 4. 一开始窗口先存储数组的第一个元素；
+     * 5. 如果窗口右端的下一个字符不在窗口中，将该字符放入窗口，右端指针指向该字符;
+     * 6. 如果窗口右端的下一个字符在窗口中，将左端指针往后移动一位，直到右端的下一个字符不在窗口中；
+     * 7. 每次右指针停止向右移动的时候，先将max与窗口的长度比较，大的保存在max中；
      * 心得总结：
-     * 执行用时：12 ms, 在所有 Java 提交中击败了26.72%的用户
-     * 内存消耗：9.1 MB, 在所有 Java 提交中击败了14.21%的用户
+     * 执行用时：9 ms, 在所有 Java 提交中击败了41.52%的用户
+     * 内存消耗：39.2 MB, 在所有 Java 提交中击败了8.52%的用户
      * 代码可以说真的是很不行了，对于基本的api，也发现忘记了很多，从10点半有思路开始尝试到2点总算完成
+     * 很愚蠢的是自己一开始使用LinkedHashMap以便保证窗口内的元素按照在字符串中的顺序继续排好序，根本就多此一举，窗口只是用来检查下一个元素存不存在
      * 最主要是居然没有想到加一个max记录所出现的最长字串长度，这一点我要用心了
      *
      * @param s
      * @return
      */
     public int lengthOfLongestSubstring(String s) {
-        int len = s.length();
-        if (len < 2) {
-            return len;
-        } else if (s != null) {
-            char[] string = s.toCharArray();
+        // 如果数组为空，返回0
+        if (s == null) {
+            return 0;
+        }
 
-            // 1. 创建一个窗口map，key为字符，value为下标
-            Map<Character, Integer> map = new LinkedHashMap<>(len);
-            // 2. 创建一个数max，记录最大值
-            int max = 0;
-            // 3. 把字符数组第一个字符添加到map中
-            map.put(string[0], 0);
-            // 4. 依次遍历字符数组
-            for (int i = 1; i < len; i++) {
-                // 5. 如果遍历到的字符在map中
-                if (map.containsKey(string[i])) {
-                    for (Iterator<Map.Entry<Character, Integer>> it = map.entrySet().iterator(); it.hasNext() && map.containsKey(string[i]); ) {
-                        Map.Entry<Character, Integer> item = it.next();
-                        // 删掉map中第一个元素，直到该字符不存在map中
-                        it.remove();
-                    }
+        // 数组长度
+        int n = s.length();
+
+        // 如果数组长度小于2，直接返回数组长度即可
+        if (n > 1) {
+            // 滑动窗口win
+            Set<Character> win = new HashSet<>(n);
+
+            // 存储最长子串长度max、窗口左端left，右端right
+            int max = 1, left = 0, right = 0;
+
+            // 保存s的第一个字符
+            win.add(s.charAt(right));
+
+            // 如果窗口右端的下一个字符在窗口中，将左端指针往后移动一位，直到右端的下一个字符不在窗口中；
+            for (; left < n; left++) {
+
+                // 如果窗口右端的下一个字符没有越界并且不在窗口中，将该字符放入窗口，右端指针指向该字符
+                while (right + 1 < n && !win.contains(s.charAt(right + 1))) {
+                    // 注意，先把right的指针向右移动，然后添加该字符
+                    win.add(s.charAt(++right));
                 }
-                // 6. 如果遍历到的字符不在map中，将其添加到map中
-                map.put(string[i], i);
-                //7. 将目前最长字串的长度保存在max中
-                max = map.size() > max ? map.size() : max;
+                // 每次右指针停止向右移动的时候，先将max与窗口的长度比较，大的保存在max中
+                max = max > win.size() ? max : win.size();
+
+                // 将左端指针往后移动一位
+                if (left != 0) {
+                    win.remove(s.charAt(left - 1));
+                }
             }
             return max;
         }
-        return 0;
-    }
-
-
-    /**
-     * 思路：滑动窗口
-     * 1. 窗口occ用HashSet实现
-     * 2.
-     *
-     * @param s
-     * @return
-     */
-    public int lengthOfLongestSubstring1(String s) {
-        // 哈希集合，记录每个字符是否出现过
-        Set<Character> occ = new HashSet<Character>();
-        int n = s.length();
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-        // ans统计最长字串
-        int rk = -1, ans = 0;
-        // 如果
-        for (int i = 0; i < n; ++i) {
-            if (i != 0) {
-                // 左指针向右移动一格，移除一个字符
-                occ.remove(s.charAt(i - 1));
-            }
-            // 当下一个字符在字符串中并且窗口中没有该字符
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                // 不断地移动右指针
-                occ.add(s.charAt(++rk));
-            }
-            // 第 i 到 rk 个字符是一个极长的无重复字符子串
-            ans = Math.max(ans, rk - i + 1);
-        }
-        return ans;
+        return n;
     }
 }
