201ba452b874454be96abeb3a51e344f64c44e71
JackLinkai <2716228597@qq.com>
2021-02-14 21:13:52 +0800

    力扣打卡

--- src/leetcode/Solution4.java
+++ src/leetcode/Solution4.java
@@ -46,22 +46,50 @@ public class Solution4 {
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+  /**
+   * 思路：
+   * 刚好最近在做有关排序算法的练习，这道题因为两个数组都是排好序了，要求出他们的中位数，
+   * 刚好可以借用递归排序的思路，把两个排好序的数组合并成一个排好序的数组nums，
+   * 如果nums的长度是单数，中位数就是最中间的那个数，
+   * 如果nums的长度是双数，中位数就是最中间的两个数的平均数
+   * 所以，只需要合并数组到要计算的那个数或者那两个数就可以了：
+   * 1. 创建一个长度为两个数组长度和的一半加一的数组nums
+   * 2. 逐个遍历这两个数组，把小的数保存到数组nums，直到保存完，注意需要有下面四个判断，并且需要对索引越界进行判断：
+   * a. 判断nums1是不是遍历完了，是的话把当前遍历到的nums2的数组保存到nums数组中，继续遍历nums2的下一个
+   * b. 判断nums2是不是遍历完了，是的话把当前遍历到的nums1的数组保存到nyms数组中，继续遍历nums1的下一个
+   * c. 如果nums1当前的数不比nums2当前的数大，就把nums1当前的数保存到nums中，继续遍历nums1的下一个
+   * d. 否则就把nums2当前的数保存到nums中，继续遍历nums2的下一个
+   * 3. 最后根据数组长度单双数计算出中位数，记得处理为double类型
+   * 执行用时： 2 ms , 在所有 Java 提交中击败了 100.00% 的用户
+   * 内存消耗： 39.7 MB , 在所有 Java 提交中击败了 65.91% 的用户
+   *
+   * @param nums1
+   * @param nums2
+   * @return
+   */
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length, point1 = 0, point2 = 0;
         double median;
         int len = n + m, num = len / 2;
+        // 1. 创建一个长度为两个数组长度和的一半加一的数组nums
         int[] nums = new int[num + 1];
+        // 2. 逐个遍历这两个数组，把小的数保存到数组nums，直到保存完，注意需要有下面四个判断，并且需要对索引越界进行判断：
         for (int i = 0; i <= num; i++) {
             if (point1 == n) {
+                // a. 判断nums1是不是遍历完了，是的话把当前遍历到的nums2的数组保存到nums数组中，继续遍历nums2的下一个
                 nums[i] = nums2[point2++];
             } else if (point2 == m) {
+                // b. 判断nums2是不是遍历完了，是的话把当前遍历到的nums1的数组保存到nyms数组中，继续遍历nums1的下一个
                 nums[i] = nums1[point1++];
             } else if (nums1[point1] <= nums2[point2]) {
+                // c. 如果nums1当前的数不比nums2当前的数大，就把nums1当前的数保存到nums中，继续遍历nums1的下一个
                 nums[i] = nums1[point1++];
             } else {
+                // d. 否则就把nums2当前的数保存到nums中，继续遍历nums2的下一个
                 nums[i] = nums2[point2++];
             }
         }
+        // 3. 最后根据数组长度单双数计算出中位数，记得处理为double类型
         if (len % 2 == 1) {
             median = nums[num] / 1.00000;
         } else {
--- src/leetcode/Solution5.java
+++ src/leetcode/Solution5.java
@@ -1,8 +1,56 @@
 package leetcode;
 
 /**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * <p>
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * <p>
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * <p>
+ * 示例 3：
+ * 输入：s = "a"
+ * 输出："a"
+ * <p>
+ * 示例 4：
+ * 输入：s = "ac"
+ * 输出："a"
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母（大写和/或小写）组成
+ *
  * @author LinKai
  * @time 2021/02/14-16:23:00
  */
 public class Solution5 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 思路：
+     * 类似这种回文等的对称数组等，都会想到使用栈解决问题
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        char[] str = s.toCharArray();
+        String string = "";
+        int n = s.length(),max=0;
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (str[i] == str[j]) {
+
+                }
+            }
+        }
+        return string;
+    }
 }
