46e9f57e605e8f620dd5937e1422a26d71e35ba1
JackLinkai <2716228597@qq.com>
2021-03-09 12:02:33 +0800

    贪心算法&动态规划

--- src/main/algorithm/dynamicprogramming/Backpack.java
+++ src/main/algorithm/dynamicprogramming/Backpack.java
@@ -1,10 +1,45 @@
 package main.algorithm.dynamicprogramming;
 
+import java.util.Arrays;
+
 /**
+ * 小偷去某商店盗窃，背有一个背包，容量是50kg，
+ * 现在有以下物品（物品不能切分,且只有一个），请问小偷应该怎么拿才能得到最大的价值？
+ * |       | 重量 | 价值  |
+ * | ----- | ---- | ----- |
+ * | 物品1 | 10kg | 60元  |
+ * | 物品2 | 20kg | 100元 |
+ * | 物品3 | 40kg | 120元 |
+ *
  * @author LinKai
  * @time 2021/03/07-16:08:00
  */
 public class Backpack {
 
+    public static void main(String[] args) {
+        // 物品对应的重量（10kg）和价值（元）
+        int[] w = {1, 2, 4}, v = {60, 100, 120};
+        // weight是背包容量（10kg）,n是物品数量
+        int weight = 5, n = 3;
+        // 一般动态规划都会需要这样一个dp，这就是我们画的那个表
+        int[][] dp = new int[n + 1][weight + 1];
+        // 每次加入的物品
+        for (int i = 1; i <= n; i++) {
+            // 分割的背包
+            for (int j = 1; j <= weight; j++) {
+                // 如果说背包可以装
+                if (w[i - 1] <= j) {
+                    // v[i-1]:当前物品的价值，dp[i-1][j-w[i-1]]:加入当前物品之前还可以存放当前物品时的背包价值
+                    // 加入物品和未加入物品进行比较
+                    dp[i][j] = Math.max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        for (int i = 0; i < n + 1; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+    }
 
 }
---
+++ src/main/algorithm/dynamicprogramming/ShoppingCart.java
@@ -0,0 +1,32 @@
+package main.algorithm.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * 双十一马上就要来了，小C心目中的女神在购物车加了N个东西，
+ * 突然她中了一个奖可以清空购物车5000元的东西（不能找零），
+ * 每个东西只能买一件，
+ * 那么她应该如何选择物品使之中奖的额度能最大利用呢？
+ * 如果存在多种最优组合你只需要给出一种即可（要求还要输出选择商品的策略）
+ *
+ * @author LinKai
+ * @time 2021/03/09-10:52:00
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+        int[] cart={500,1200,1500,2400,3200};
+        // n是商品总数,m是清空购物车的额度
+        int n=5,m=5000;
+        int[][] dp=new int[n+1][m+1];
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if (cart[i-1]<=j){
+                    dp[i][j]=Math.max(cart[i-1]+dp[i-1][j-cart[i-1]],dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
+}
